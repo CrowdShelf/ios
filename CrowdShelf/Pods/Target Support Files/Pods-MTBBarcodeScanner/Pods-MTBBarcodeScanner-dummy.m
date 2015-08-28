36545cf53b4dbff95795871b438685dcb0f2dd9f
Øyvind Grimnes <oyvindkg@yahoo.com>
2015-08-28 13:29:21 +0200

    Cocoapods update

--- CrowdShelf/Pods/Target Support Files/Pods-MTBBarcodeScanner/Pods-MTBBarcodeScanner-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MTBBarcodeScanner : NSObject
-@end
-@implementation PodsDummy_Pods_MTBBarcodeScanner
-@end
--- CrowdShelf/Pods/Target Support Files/Pods-MTBBarcodeScanner/Pods-MTBBarcodeScanner-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "MTBBarcodeScanner.h"
-
-FOUNDATION_EXPORT double MTBBarcodeScannerVersionNumber;
-FOUNDATION_EXPORT const unsigned char MTBBarcodeScannerVersionString[];
-
